c597771d9380a26fca9f4f26a108e3a0e7b4379f
anderfernandez <59762055+anderfernandez@users.noreply.github.com>
2020-10-26 22:00:33 +0100

    Update app.R

--- Prediccion de la cotizacion/Demo/app.R
+++ Prediccion de la cotizacion/Demo/app.R
@@ -23,13 +23,6 @@ server <- function(input, output) {
         datos()
     )
 
-    output$distPlot <- renderPlot({
-        
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = 10 + 1)
-
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
 }
 
 shinyApp(ui = ui, server = server)
